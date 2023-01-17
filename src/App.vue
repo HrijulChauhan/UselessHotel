db4cc6ccc010448c4f3239aa348d9cb4c9522435
Hrijul Chauhan <chauhanhrijul4@gmail.com>
2023-01-17 23:25:06 +0530

    reworked the landing page

--- src/App.vue
+++ src/App.vue
@@ -5,50 +5,57 @@
 
   <div class="grandparent-wrapper pb-96">
     <!-- NAVBAR -->
-    <div class="navbar flex justify-around px-52">
+    <div class="navbar-wrap flex justify-center">
+      <div class="navbar flex justify-between">
 
-      <div class="logo text-3xl pt-8 -mr-0 text-white">
-        GB
-      </div>
-
-      <div class="links text-xs flex pt-8 items-center text-gray-200">
-        <a class="mr-6 ml-14 hover:text-white" href="">About Us</a>
-        <a class="mr-6 hover:text-white" href="">Contact Us</a>
-        <a class="mr-6 hover:text-white" href="">Testimonials</a>
-        <a class=" hover:text-white" href="">Locations</a>
-      </div>
-
-      <div class="mr-0 pt-8 text-xs">
-        <button class="log-in px-7 py-2">Log in</button>
-      </div>
-
-    </div>
-
-
-    <!-- HERO SECTION  -->
-    <div class="parent-wrapper">
-      <div class="wrapper flex  flex-col ">
-
-        <div class="hero-text text-white text-center text-8xl mt-24 mr-0">
-          <!-- <span class="visit text-5xl">Visit</span> --> Grand<br> 
-          <span class="text-white vercel"> Budapest </span>
+        <div class="logo text-3xl pt-8 text-black ml-1">
+          GB
         </div>
 
-        <div class="sub-text text-center text-gray-400 mr-0 mt-5 text-base">
-          Experience luxury and elegance at the Grand Budapest Hotel   <br>
+        <div class="links text-xs flex pt-8  items-center text-gray-400 tracking-tight ">
+          <a class="mr-0 px-3  hover:text-black" href="">About Us</a>
+          <a class="mr-0 px-3 hover:text-black" href="">Contact Us</a>
+          <a class="mr-0 px-3 hover:text-black" href="">Testimonials</a>
+          <a class="px-3 hover:text-black" href="">Locations</a>
+        </div>
+
+        <div class="mr-0 pt-10 text-sm text-black">
+          <!-- <button class="log-in px-7 py-2">Log in</button> -->
+          <div class="log-in">Log in</div>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- HERO SECTION  -->
+    <div class="parent-wrapper flex">
+      <div class="wrapper flex  flex-col">
+
+        <div class="hero-text text-black text-left text-9xl mt-28">
+          <!-- <span class="visit text-5xl">Visit</span> --> Grand<br>
+          <span class="text-black vercel"> Budapest </span>
+        </div>
+
+        <div class="sub-text text-left text-gray-400 ml-2 mt-5 text-base tracking-tight">
+          Experience luxury and elegance at the <span class="text-green-600 font-medium"> Grand Budapest Hotel </span>
+          <br>
           a royal sanctuary for the discerning traveler
         </div>
 
-        <div class="pt-8 text-xs mr-0 text-center">
+        <div class="pt-8 text-xs  text-left ml-2">
           <button class="discover px-8 py-3">Discover</button>
         </div>
       </div>
 
+
+      <div class="hero-picture "></div>
     </div>
+  
   </div>
 
+
   <!-- BODY SECTION  -->
-  <div class="promo-wrap flex justify-center mt-12">
+  <div class="promo-wrap flex justify-center -mt-5">
     <div class="promotion-block text-white text-4xl p-10">
       Find your dream location
       <div class="promo-subtext text-base text-gray-400 pt-2">
@@ -77,7 +84,7 @@
 
 <style scoped>
 .navbar {
-  width: 100%;
+  width: 70%;
   /* height: 2em; */
   /* border: 5px blue solid; */
 }
@@ -88,7 +95,7 @@
 
 .links {
   font-family: 'Inter';
-  font-weight: 500;
+  font-weight: 400;
   font-size: 13px;
   transition: ease-in-out;
   animation-duration: 0.5s;
