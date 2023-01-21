c0e9bebd2e7dd446a4de274dcfba84bb250c43cb
Hrijul Chauhan <chauhanhrijul4@gmail.com>
2023-01-22 00:46:04 +0530

    added activities

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,11 +2,11 @@
 
 <template>
 
-<!-- NAVBAR + HERO  -->
-  <div class="grandparent-wrapper pb-96">
+  <!-- NAVBAR + HERO  -->
+  <div class="grandparent-wrapper pb-96 ">
 
     <!-- NAVBAR -->
-    <div class="navbar-wrap flex justify-center">
+    <div class="animate-1 navbar-wrap flex justify-center">
       <div class="navbar flex justify-between">
 
         <div class="logo text-3xl pt-8 text-black ml-1">
@@ -32,25 +32,25 @@
     <div class="parent-wrapper flex">
       <div class="wrapper flex  flex-col">
 
-        <div class="hero-text text-black text-left text-9xl mt-28">
+        <div class="hero-text text-black text-left text-9xl mt-28 animate-1">
           <!-- <span class="visit text-5xl">Visit</span> --> Grand<br>
           <span class="text-black vercel"> Budapest </span>
         </div>
 
-        <div class="sub-text text-left text-gray-400 ml-2 mt-5 text-lg tracking-tight">
+        <div class="sub-text animate-1_25 text-left text-gray-400 ml-2 mt-5 text-lg tracking-tight">
           Experience luxury and elegance at the <span class="text-green-600 font-medium"> Grand Budapest Hotel </span>
           <br>
           a royal sanctuary for the discerning traveler
         </div>
 
-        <div class="pt-8 text-base text-left ml-2">
+        <div class="pt-8 text-base text-left ml-2 animate-1_5">
           <button class="discover px-8 py-3">Discover</button>
         </div>
       </div>
 
 
       <!-- Main Lockup Picture  -->
-      <div class="hero-picture "></div>
+      <div class="hero-picture animate-1_25"></div>
     </div>
 
   </div>
@@ -59,235 +59,255 @@
   <!-- BODY SECTION  -->
 
   <div class="body">
-  <!-- Promotion block code  -->
-  <div class="promo-wrap flex justify-center -mt-5 ml-12">
-    <div class="promotion-block text-white text-4xl p-14">
-      Find your dream location
-      <div class="promo-subtext text-base text-gray-400 pt-2">
-        With over 800 locations in 37 countries, you're sure to find what <br>
-        you're looking for. Find what you're looking for, and discover <br>
-        the heaven that is The Grand Budapest.
-      </div>
-      <div class="see-flex flex mt-4">
-        <div class="promo-subtext text-base see-more pt-5">
-          See more
+    <!-- Promotion block code  -->
+    <div class="animate-1_5 promo-wrap flex justify-center -mt-5 ml-12">
+      <div class="promotion-block text-white text-4xl p-14">
+        Find your dream location
+        <div class="promo-subtext text-base text-gray-400 pt-2">
+          With over 800 locations in 37 countries, you're sure to find what <br>
+          you're looking for. Find what you're looking for, and discover <br>
+          the heaven that is The Grand Budapest.
         </div>
-        <div class="right-arrow pt-5 pl-2 text-base">
-          →
+        <div class="see-flex flex mt-4">
+          <div class="promo-subtext text-base see-more pt-5">
+            See more
+          </div>
+          <div class="right-arrow pt-5 pl-2 text-base">
+            →
+          </div>
         </div>
       </div>
+
+      <br>
+      <br>
+      <div class="mt-96"></div>
+
     </div>
-
-    <br>
-    <br>
-    <div class="mt-96"></div>
-
-  </div>
-  <!-- Promotion End  -->
+    <!-- Promotion End  -->
 
 
-  <!-- Premier Collection Heading  -->
-  <div class="premier-collection mt-10">
+    <!-- Premier Collection Heading  -->
+    <div class="premier-collection mt-10">
 
-    <div class="premier-heading text-6xl tracking-tight">
-      The Premier Collection
-    </div>
-    <div class="premier-subtext mt-3 text-gray-400 text-lg tracking-normal">
-      <!-- Handpicked for their exceptional amenities, personalized service, and prime locations, 
+      <div class="premier-heading text-6xl tracking-tight">
+        The Premier Collection
+      </div>
+      <div class="premier-subtext mt-3 text-gray-400 text-lg tracking-normal">
+        <!-- Handpicked for their exceptional amenities, personalized service, and prime locations, 
       these hotels are designed to provide you with an unforgettable and elevated experience.  -->
-      Whether you're traveling for business or leisure, our team at the<span class="text-green-600 font-medium"> Grand
-        Budapest Premier Collection </span><br>
-      hotels offer the perfect blend of comfort and elegance, ensuring that your stay
-      to be truly special.
+        Whether you're traveling for business or leisure, our team at the<span class="text-green-600 font-medium"> Grand
+          Budapest Premier Collection </span><br>
+        hotels offer the perfect blend of comfort and elegance, ensuring that your stay
+        to be truly special.
+      </div>
+
+    </div>
+
+
+    <div class="premier-wrapper flex justify-center">
+
+
+      <!-- ACTUAL PREMIER HOTELS  -->
+      <div class="premier-hotels mt-10">
+
+
+
+
+
+
+        <!-- FIRST ROW  -->
+        <div class="first-row flex justify-between">
+
+          <!-- MUMBAI  -->
+          <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
+            <div class="hotel-illustration mr-5 first">
+
+            </div>
+            <div class="text-xl font-medium tracking-tight -mt-1">
+              Mumbai, India
+              <div class="text-sm text-gray-400 font-normal tracking-normal">
+                Mumbai, or Bombay,<br>
+                known for it's delicious <br>
+                food and street life.
+              </div>
+
+              <div class="see-flex flex">
+                <div class="text-sm mt-3">
+                  See more
+                </div>
+                <div class="arr  text-base ml-2">
+                  →
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- PARIS FRANCE  -->
+          <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
+            <div class="hotel-illustration mr-5 second">
+
+            </div>
+            <div class="text-xl font-medium tracking-tight -mt-1 ">
+              Paris, France
+              <div class="text-sm text-gray-400 font-normal tracking-normal">
+                <!-- Paris, known for romantic <br> -->
+                Paris, known for romantic <br>
+                ambiance and it's <br>
+                delicious culinary scene
+              </div>
+              <div class="see-flex flex">
+                <div class="text-sm mt-3">
+                  See more
+                </div>
+                <div class="arr  text-base ml-2">
+                  →
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- NEW YORKKK CONCRETE JUNGLES WHERE DREAMS ARE MADE OF  -->
+          <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
+            <div class="hotel-illustration mr-5 third">
+
+            </div>
+            <div class="text-xl font-medium -mt-1  tracking-tight">
+              New York, USA
+              <div class="text-sm text-gray-400 font-normal tracking-normal">
+                The Big Apple, famous <br>
+                for its iconic skyline <br>
+                and diverse culture.
+              </div>
+              <div class="see-flex flex">
+                <div class="text-sm mt-3">
+                  See more
+                </div>
+                <div class="arr  text-base ml-2">
+                  →
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+
+        <!-- SECOND ROW  -->
+        <div class="second-row flex justify-between mt-5">
+
+          <!-- tokyo japan  -->
+          <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
+            <div class="hotel-illustration mr-5 fourth">
+
+            </div>
+            <div class="text-xl font-medium tracking-tight -mt-1">
+              Tokyo, Japan
+              <div class="text-sm text-gray-400 font-normal tracking-normal">
+                The perfect blend of <br>
+                traditional and modern <br>
+                century culture
+              </div>
+
+              <div class="see-flex flex">
+                <div class="text-sm mt-3">
+                  See more
+                </div>
+                <div class="arr  text-base ml-2">
+                  →
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- TURKEY ISTANBUL  -->
+          <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
+            <div class="hotel-illustration mr-5 fifth">
+
+            </div>
+            <div class="text-xl font-medium tracking-tight -mt-1 ">
+              Turley, Istanbul
+              <div class="text-sm text-gray-400 font-normal tracking-normal">
+                <!-- Paris, known for romantic <br> -->
+                Known for a rich history <br>
+                and culture, from Blue <br>
+                Mosque to Grand Bazaar
+              </div>
+              <div class="see-flex flex">
+                <div class="text-sm mt-3">
+                  See more
+                </div>
+                <div class="arr  text-base ml-2">
+                  →
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- SYDNEY AUSTRALIA  -->
+          <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
+            <div class="hotel-illustration mr-5  sixth">
+
+            </div>
+            <div class="text-xl font-medium -mt-1  tracking-tight">
+              Sydney, AUS
+              <div class="text-sm text-gray-400 font-normal tracking-normal">
+                Notable landmarks, <br>
+                like the Sydney Opera<br>
+                House, Harbour Bridge
+              </div>
+              <div class="see-flex flex">
+                <div class="text-sm mt-3">
+                  See more
+                </div>
+                <div class="arr  text-base ml-2">
+                  →
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+
+        <!-- Premier Hotel wrap end  -->
+      </div>
+
+
+
     </div>
 
   </div>
 
 
-  <div class="premier-wrapper flex justify-center">
-
-
-    <!-- ACTUAL PREMIER HOTELS  -->
-    <div class="premier-hotels mt-10">
-
-
-
-
-
-
-      <!-- FIRST ROW  -->
-      <div class="first-row flex justify-between">
-
-        <!-- MUMBAI  -->
-        <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
-          <div class="hotel-illustration mr-5 first">
-
-          </div>
-          <div class="text-xl font-medium tracking-tight -mt-1">
-            Mumbai, India
-            <div class="text-sm text-gray-400 font-normal tracking-normal">
-              Mumbai, or Bombay,<br>
-              known for it's delicious <br>
-              food and street life.
-            </div>
-
-            <div class="see-flex flex">
-              <div class="text-sm mt-3">
-                See more
-              </div>
-              <div class="arr  text-base ml-2">
-                →
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- PARIS FRANCE  -->
-        <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
-          <div class="hotel-illustration mr-5 second">
-
-          </div>
-          <div class="text-xl font-medium tracking-tight -mt-1 ">
-            Paris, France
-            <div class="text-sm text-gray-400 font-normal tracking-normal">
-              <!-- Paris, known for romantic <br> -->
-              Paris, known for romantic <br>
-              ambiance and it's <br>
-              delicious culinary scene
-            </div>
-            <div class="see-flex flex">
-              <div class="text-sm mt-3">
-                See more
-              </div>
-              <div class="arr  text-base ml-2">
-                →
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- NEW YORKKK CONCRETE JUNGLES WHERE DREAMS ARE MADE OF  -->
-        <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
-          <div class="hotel-illustration mr-5 third">
-
-          </div>
-          <div class="text-xl font-medium -mt-1  tracking-tight">
-            New York, USA
-            <div class="text-sm text-gray-400 font-normal tracking-normal">
-              The Big Apple, famous <br>
-              for its iconic skyline <br>
-              and diverse culture.
-            </div>
-            <div class="see-flex flex">
-              <div class="text-sm mt-3">
-                See more
-              </div>
-              <div class="arr  text-base ml-2">
-                →
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-
-
-      <!-- SECOND ROW  -->
-      <div class="second-row flex justify-between mt-5">
-
-        <!-- tokyo japan  -->
-        <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
-          <div class="hotel-illustration mr-5 fourth">
-
-          </div>
-          <div class="text-xl font-medium tracking-tight -mt-1">
-            Tokyo, Japan
-            <div class="text-sm text-gray-400 font-normal tracking-normal">
-              The perfect blend of <br>
-              traditional and modern <br>
-              century culture
-            </div>
-
-            <div class="see-flex flex">
-              <div class="text-sm mt-3">
-                See more
-              </div>
-              <div class="arr  text-base ml-2">
-                →
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- TURKEY ISTANBUL  -->
-        <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
-          <div class="hotel-illustration mr-5 fifth">
-
-          </div>
-          <div class="text-xl font-medium tracking-tight -mt-1 ">
-            Turley, Istanbul
-            <div class="text-sm text-gray-400 font-normal tracking-normal">
-              <!-- Paris, known for romantic <br> -->
-              Known for a rich history <br>
-              and culture, from Blue <br>
-              Mosque to Grand Bazaar
-            </div>
-            <div class="see-flex flex">
-              <div class="text-sm mt-3">
-                See more
-              </div>
-              <div class="arr  text-base ml-2">
-                →
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- SYDNEY AUSTRALIA  -->
-        <div class="hotel-wrapper flex hover:shadow-2xl p-12 rounded-xl">
-          <div class="hotel-illustration mr-5  sixth">
-
-          </div>
-          <div class="text-xl font-medium -mt-1  tracking-tight">
-            Sydney, AUS
-            <div class="text-sm text-gray-400 font-normal tracking-normal">
-              Notable landmarks, <br>
-              like the Sydney Opera<br>
-              House, Harbour Bridge
-            </div>
-            <div class="see-flex flex">
-              <div class="text-sm mt-3">
-                See more
-              </div>
-              <div class="arr  text-base ml-2">
-                →
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-
-
-      <!-- Premier Hotel wrap end  -->
-    </div>
-
-
-
-  </div>
-
-</div>
-
-
-  <div class="activity-heading">
+  <!-- DISCOVER ACTIVITIES PORTION  -->
+  <div class="activity-heading mt-20 text-6xl tracking-tight">
     Discover Activities
   </div>
-
-
-  <div>
-    BRuh
+  <div class="premier-subtext mt-3 text-gray-400 text-lg tracking-normal">
+    <!-- Handpicked for their exceptional amenities, personalized service, and prime locations, 
+      these hotels are designed to provide you with an unforgettable and elevated experience.  -->
+    Discover a world of adventure and excitement with our wide
+    range of activities at the
+    <span class="text-green-600 font-medium"> Grand Budapest Premier Collection</span> <br>
+    hotels. From thrilling outdoor activities to cultural experiences,
+    our team will help you make the most of your stay.
   </div>
+
+
+  <div class="activity-wrap flex justify-between mt-14 ">
+    
+    <div class="activity-box first-activity ">
+    </div>
+
+    <div class="activity-box second-activity">
+    </div>
+
+    <div class="activity-box third-activity">
+    </div>
+
+  </div>
+
 
 
 
@@ -343,7 +363,7 @@
 
 
 <script>
-
+console.log(document.documentElement.clientWidth);
 
 
 </script>
