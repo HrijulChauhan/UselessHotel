5fda51ba9b1940609891974571d3a457c145dcd9
Hrijul Chauhan <chauhanhrijul4@gmail.com>
2023-01-28 03:18:42 +0530

    Managed to connect the FORM.vue component to the Java API and store all info on mySQL db

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -14,13 +14,13 @@
 
       <div class="flex flex-col">
         <label for="">First Name</label>
-        <input type="text" class=" bg-gray-200 mt-2 rounded-md p-2" style="width:300px; height: 50px;">
+        <input type="text" class=" bg-gray-200 mt-2 rounded-md p-2" style="width:300px; height: 50px;" v-model="firstName">
       </div>
 
 
       <div class="flex flex-col pl-16">
         <label for="">Last Name</label>
-        <input type="text" class="bg-gray-200 mt-2 rounded-md p-2" style="width:300px; height: 50px;">
+        <input type="text" class="bg-gray-200 mt-2 rounded-md p-2" style="width:300px; height: 50px;" v-model="lastName">
       </div>
 
     </div>
@@ -51,9 +51,9 @@
 
       <div class="flex flex-col pl-16 mt-10">
         <label for="">Enter number of rooms</label>
-        <input type="text" class="bg-gray-200 mt-2 rounded-md p-2" style="width:300px; height: 50px;">
+        <!-- <p>{{ NumberOfRooms }}</p> -->
+        <input type="number" class="bg-gray-200 mt-2 rounded-md p-2" style="width:300px; height: 50px;" v-model="NumberOfRooms">
       </div>
-
     </div>
 
 
@@ -62,15 +62,15 @@
       <div class="col-span-6 sm:col-span-3 mt-10 " style="width:300px; height: 50px;">
 
         <label for="country" class="block text-sm font-medium text-gray-700 mt-2">Package Option at {{
-          selectedLocation
+          amenities
         }}</label>
         <select id="country" name="country" autocomplete="country-name"
-          class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+          class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" v-model="amenities">
 
-          <option>None</option>
-          <option>Breakfast included</option>
-          <option>Breakfast + Dinner</option>
-          <option>Breakfast, Dinner + Unlimited Drinks</option>
+          <option value="none">None</option>
+          <option value="breakfast">Breakfast included</option>
+          <option value="breakfast + dinner">Breakfast + Dinner</option>
+          <option value="breakfast, dinner + unlimited drinks">Breakfast, Dinner + Unlimited Drinks</option>
         </select>
 
 
@@ -79,15 +79,15 @@
       <!-- room type + rate -->
       <div class="col-span-6 sm:col-span-3 mt-10 pl-16">
 
-        <label for="country" class="block text-sm font-medium text-gray-700 mt-2">Room type (per night)</label>
+        <label for="country" class="block text-sm font-medium text-gray-700 mt-2">Room type (per night) {{ roomRate }}</label>
         <select id="country" name="country" autocomplete="country-name"
           class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-          style="width:300px" v-if="selectedLocation == 'Mexico'">
+          style="width:300px" v-model="roomRate">
 
-          <option>Single Bedroom 50$</option>
-          <option>Double Bedroom 75$</option>
-          <option>Suite 125$</option>
-          <option>Penthouse 250$</option>
+          <option value="50">Single Bedroom 50$</option>
+          <option value="75">Double Bedroom 75$</option>
+          <option value="125">Suite 125$</option>
+          <option value="250">Penthouse 250$</option>
         </select>
 
 
@@ -98,6 +98,8 @@
     <div class="text-sm mt-16">
       <button class="font-medium px-8 py-3 bg-black text-white" @click="postToApi()">Browse</button>
     </div>
+    
+
 
   </form>
 </template>
@@ -111,26 +113,38 @@ import axios from 'axios'
 export default {
   data() {
     return {
+      firstName:'',
+      lastName:'',
       selectedLocation: '',
+      NumberOfRooms: 0,
+      amenities:'',
+      roomRate: 0,
     }
   },
+
+
   methods: {
+    
     postToApi() {
       axios.post('http://localhost:8080/api/v1/hotel', {
 
-        "name": "This is a POST from vue",
-        "location": "Budapest",
-        "description": " BRUH 5-star hotel in the heart of Budapest",
-        "numberOfRooms": 2,
-        "amenities": "Spa, fitness center, room service, concierge",
-        "roomRate": 150.50
+        "name": this.firstName + " " + this.lastName,
+        "location": this.selectedLocation,
+        "numberOfRooms": this.NumberOfRooms,
+        "description": "",
+        "amenities": this.amenities,
+        "roomRate": this.roomRate
       
         }
       )
+    },
+
+    validateNumberOfRooms(e){
+      this.NumberOfRooms = parseInt(e.target.value)
+      console.log(NumberOfRooms)
     }
   }
 }
 
-// console.log(selectedLocation)
 
 </script>
