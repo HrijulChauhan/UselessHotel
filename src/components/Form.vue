b7415031e6ab079ab97b84c62bc4e5a703a7c580
Hrijul Chauhan <chauhanhrijul4@gmail.com>
2023-01-27 14:51:43 +0530

    figured out how to POST

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -32,7 +32,8 @@
 
         <label for="country" class="block text-sm font-medium text-gray-700 mt-2">Country</label>
         <select id="country" name="country" autocomplete="country-name"
-          class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" v-model="selectedLocation">
+          class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          v-model="selectedLocation">
 
           <option value="New York">New York</option>
           <option value="Canada">Canada</option>
@@ -60,7 +61,9 @@
       <!-- PACKAGE OPTION  -->
       <div class="col-span-6 sm:col-span-3 mt-10 " style="width:300px; height: 50px;">
 
-        <label for="country" class="block text-sm font-medium text-gray-700 mt-2">Package Option at {{ selectedLocation }}</label>
+        <label for="country" class="block text-sm font-medium text-gray-700 mt-2">Package Option at {{
+          selectedLocation
+        }}</label>
         <select id="country" name="country" autocomplete="country-name"
           class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
 
@@ -78,7 +81,8 @@
 
         <label for="country" class="block text-sm font-medium text-gray-700 mt-2">Room type (per night)</label>
         <select id="country" name="country" autocomplete="country-name"
-          class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" style="width:300px">
+          class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          style="width:300px" v-if="selectedLocation == 'Mexico'">
 
           <option>Single Bedroom 50$</option>
           <option>Double Bedroom 75$</option>
@@ -90,15 +94,39 @@
       </div>
     </div>
 
+
+    <div class="text-sm mt-16">
+      <button class="font-medium px-8 py-3 bg-black text-white" @click="postToApi()">Browse</button>
+    </div>
+
   </form>
 </template>
 
-<script>
 
-export default{
+
+<script>
+import axios from 'axios'
+
+
+export default {
   data() {
     return {
       selectedLocation: '',
+    }
+  },
+  methods: {
+    postToApi() {
+      axios.post('http://localhost:8080/api/v1/hotel', {
+
+        "name": "This is a POST from vue",
+        "location": "Budapest",
+        "description": " BRUH 5-star hotel in the heart of Budapest",
+        "numberOfRooms": 2,
+        "amenities": "Spa, fitness center, room service, concierge",
+        "roomRate": 150.50
+      
+        }
+      )
     }
   }
 }
