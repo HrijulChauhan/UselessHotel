fc0c7b96dc3b0c413f1a69cf29668b7e25d83ea3
Hrijul Chauhan <chauhanhrijul4@gmail.com>
2023-03-01 22:27:23 +0530

    figured out axios

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -3,17 +3,41 @@
         <h2>Hi!</h2>
         the route parameter is
         {{ id }}
+        {{ hotelInformation[0].roomRate }}
     </div>
+
+    <button @click="sendPostRequest" class="bg-red-400">
+        this is the button
+    </button>
 </template>
 
 <script>
+import axios from 'axios'
 
-export default{
-    data(){
-        return{
+export default {
+    data() {
+        return {
             id: this.$route.params.id,
-        }   
+            url: 'http://localhost:8080/api/v1/hotel/' + this.$route.params.id, 
+            hotelInformation: null,
+        }
+    },
+    methods: {
+        sendPostRequest(){
+            axios.get(this.url)
+            .then(response => {
+                console.log(response.data);
+                this.hotelInformation = response.data;
+            })
+            .catch(error => {
+                console.log(error)
+            });
+        
+            console.log(this.url)
+        },
     }
 }
+
+
 
 </script>
