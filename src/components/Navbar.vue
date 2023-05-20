844c65f9fcd11424f0dc03e273729a009dc6d703
HrijulChauhan <hrijulchauhan@gmail.com>
2023-05-20 13:45:48 +0530

    margin shenangin

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-blue-400 IBM text-center text-white py-1 text-xs tracking-tight font-medium">CHECK OUT PREMIER HOTELS FOR THE BEST OF OUR COLLECTIONS
   </header>
-  <header class="grid mb-64 md:mb-0 md:h-14 grid-cols-3 grid-rows-1 items-center text-base text-white transition-colors delay-150 ease-in-out hover:bg-amber-50 hover:text-black">
+  <header class="grid mb-64 py-5 md:py-0 md:mb-0 md:h-14 grid-cols-3 grid-rows-1 items-center text-base text-white transition-colors delay-150 ease-in-out hover:bg-amber-50 hover:text-black">
     
     <div class="flex justify-start font-light IBM invisible md:visible">
       <div class="pl-20 pr-10 IBM">Browse</div>
