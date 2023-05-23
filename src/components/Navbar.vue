de96cafcedf5e8794d6fd0a365f4cf0cec6a6052
HrijulChauhan <hrijulchauhan@gmail.com>
2023-05-23 17:25:39 +0530

    some more main page stuff

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Promo header -->
-  <header class="IBM bg-blue-400 py-1 items-center text-center text-sm font-medium tracking-tight text-white">NEW YORK LOCATIONS NOW AVAILABLE</header>
+  <header class="IBM bg-indigo-200 py-1 items-center text-center text-sm font-medium tracking-tight text-[#1b254b]">NEW LOCATIONS NOW AVAILABLE</header>
 
   <header class="grid grid-cols-3 grid-rows-1 items-center h-[15vh] text-base text-white transition-colors delay-150 ease-in-out hover:bg-amber-50 hover:text-black md:mb-0 md:h-14 md:py-0">
     <div class="IBM invisible flex justify-start font-light md:visible">
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,27 +1,49 @@
 <template>
-  
-  <body class="relative h-[90vh] bg-black bg-opacity-25">
-  
+  <section class="relative h-[90vh]">
     <video src="/bgvideo.mp4" autoplay loop muted class="absolute top-0 left-0 -z-10 h-[90vh] w-full object-cover"></video>
     <navbar />
 
     <!-- Main Lockup -->
     <div class="absolute bottom-10 left-0 right-0 text-center md:text-left">
-      
-      <p class="ITC text-6xl text-left text-white w-[85%] mx-auto md:pl-20 md:w-full md:text-left md:text-7xl lg:text-8xl xl:text-9xl">
+      <p class="ITC mx-auto w-[85%] text-left text-6xl text-amber-50 md:w-full md:pl-20 md:text-left md:text-7xl lg:text-8xl xl:text-9xl">
         The best way to stay <span class="ITC underline">happy</span> <br />
         for a dream <span class="ITC underline">vacation</span>
       </p>
-      
-      <p class="IBM  text-sm text-left text-gray-200 mt-7 w-[85%] tracking-wider leading-relaxed mx-auto md:hidden">
-        Experience uncomprimising luxury, with none of the stress
-      </p>
-      
-      <button class="IBM mt-7 rounded-full font-medium bg-white py-3 w-[85%] md:w-72 md:ml-20">EXPLORE MORE</button>
-    </div>
-  
-  </body>
 
+      <p class="IBM mx-auto mt-7 w-[85%] text-left text-sm leading-relaxed tracking-wider md:hidden">Experience uncomprimising luxury, with none of the stress</p>
+
+      <button class="IBM mt-7 w-[85%] rounded-full bg-amber-50 py-3 font-medium md:ml-20 md:w-72">EXPLORE MORE</button>
+    </div>
+  </section>
+
+  <section>
+    <p class="ITC pt-16 text-center text-4xl md:text-7xl">A place for all your desires</p>
+    <p class="IBM group mt-2 text-center tracking-tight text-gray-700 transition duration-200 ease-in-out hover:text-blue-700">
+      Browse more
+      <span aria-hidden="true" class="inline-block translate-x-0 transition-transform duration-200 ease-in-out group-hover:translate-x-1">→</span>
+    </p>
+
+    <div class="mx-auto mt-10 grid w-[85vw] grid-cols-1 grid-rows-1 gap-10 pb-96 text-center lg:w-full lg:grid-cols-4 lg:pl-20 lg:pr-20">
+      <div class="h-96 lg:h-full">
+        <img src="https://imgur.com/JClCEA3.jpg" alt="" class="outline-3 mb-4 h-full w-full rounded-2xl object-cover outline-dashed outline-black" />
+        <p class="IBM lg:text-sm xl:text-base">Central Park, New York</p>
+      </div>
+
+      <div class="mt-5 hidden h-96 lg:mt-0 lg:block lg:h-full">
+        <img src="https://imgur.com/Th6NDW5.jpg" alt="" class="outline-3 mb-4 h-full w-full rounded-2xl object-cover outline-dashed outline-black" />
+        <p class="IBM lg:text-sm xl:text-base">New Delhi, India</p>
+      </div>
+
+      <div class="hidden h-96 lg:mt-0 lg:block lg:h-full">
+        <img src="https://imgur.com/JiqBFcZ.jpg" alt="" class="outline-3 mb-4 h-full w-full rounded-2xl object-cover outline-dashed outline-black" />
+        <p class="IBM lg:text-sm xl:text-base">Grand Bazaar, Turkey</p>
+      </div>
+      <div class="mt-8 h-96 lg:mt-0 lg:block lg:h-full">
+        <img src="https://imgur.com/n0MkUpE.jpg" alt="" class="outline-3 mb-4 h-full w-full rounded-2xl object-cover outline-dashed outline-black" />
+        <p class="IBM lg:text-sm xl:text-base">Florence, Italy</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style>
